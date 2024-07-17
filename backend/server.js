b2a442057d0e36d4bdeb89d2c62c0ddb72ce4d49
Evan Knight <evanknight18@gmail.com>
2024-07-17 11:09:04 -0600

    Prepare for Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-// backend/server.js
-
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-const taskRoutes = require('./routes/taskRoutes'); // Ensure this line is present
+const taskRoutes = require('./routes/taskRoutes');
+const path = require('path');
 
 const app = express();
 
@@ -15,7 +14,17 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes); // Ensure this line is present
+app.use('/api/tasks', taskRoutes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
